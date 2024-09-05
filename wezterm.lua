e1f8d20ef300036c25fce7ddfbd67db7ac8701bd
Johan Xie <johanpoisson@outlook.com>
2024-09-06 00:50:57 +0800

    feat: change color scheme, add sync with os appearance, update fonts and adjust paddings

--- wezterm.lua
+++ wezterm.lua
@@ -19,24 +19,50 @@ if wezterm.gui then
   })
 end
 
-local mono_font = wezterm.font_with_fallback({
-  "Monaspace Neon",
-  "Symbols Nerd Font",
-  "Noto Sans CJK SC",
-})
+-- Font settings
+local font_table = {
+  normal = { "Monaspace Neon", "Noto Sans CJK SC", "Symbols Nerd Font" },
+  pixel = {
+    "Departure Mono",
+    "Ark Pixel 12px Proportional zh_cn",
+    "Unifont",
+    "Symbols Nerd Font",
+  },
+}
+
+local selected_font = wezterm.font_with_fallback(font_table.pixel)
+
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+local function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return "Dark"
+end
+
+-- Sync with OS
+local function scheme_for_appearance(appearance)
+  if appearance:find("Dark") then
+    return "Lettepa Dark"
+  else
+    return "Lettepa Light"
+  end
+end
 
 return {
   -- UI
   adjust_window_size_when_changing_font_size = false,
-  color_scheme = "Matataki", -- or "Kagayaki" if in light mode
+  color_scheme = scheme_for_appearance(get_appearance()),
   enable_scroll_bar = true,
-  font = mono_font,
+  font = selected_font,
   font_size = 20,
   tab_max_width = 40,
   use_fancy_tab_bar = false,
   window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+  window_padding = { top = 0, bottom = 0, left = 8 },
   -- Key map
-  leader = { key = "Enter", mods = "CTRL", timeout_milliseconds = 1000 },
+  leader = { key = "'", mods = "CTRL", timeout_milliseconds = 1000 },
   keys = {
     {
       key = "y",
