d1bdd6b464b41984f691177a92495d9e2a0b8915
Johan Xie <johanpoisson@outlook.com>
2025-01-03 19:58:34 +0800

    feat: upgrade to config builder and add os related settings

--- wezterm.lua
+++ wezterm.lua
@@ -1,35 +1,33 @@
 local wezterm = require("wezterm")
 local act = wezterm.action
+local config = wezterm.config_builder()
 
--- Modify copy mode
-local copy_mode = nil
-if wezterm.gui then
-  copy_mode = wezterm.gui.default_key_tables().copy_mode
-  table.insert(
-    copy_mode,
-    { key = "[", mods = "CTRL", action = act.CopyMode("Close") }
-  )
-  table.insert(copy_mode, {
-    key = "Enter",
-    mods = "NONE",
-    action = act.Multiple({
-      { CopyTo = "ClipboardAndPrimarySelection" },
-      { CopyMode = "Close" },
-    }),
-  })
+--
+-- OS related
+--
+
+local is_linux = wezterm.target_triple:find("linux") ~= nil
+local is_darwin = wezterm.target_triple:find("darwin") ~= nil
+local is_windows = wezterm.target_triple:find("windows") ~= nil
+
+if is_windows then
+  config.default_prog = { "pwsh", "--NoLogo" }
 end
+
+--
+-- UI
+--
 
 -- Font settings
 local font_table = {
   normal = { "Monaspace Neon", "Noto Sans CJK SC", "Symbols Nerd Font" },
   pixel = {
     "Departure Mono",
-    "Ark Pixel 12px Proportional zh_cn",
+    -- "Ark Pixel 12px Proportional zh_cn",
     "Unifont",
     "Symbols Nerd Font",
   },
 }
-
 local selected_font = wezterm.font_with_fallback(font_table.pixel)
 
 -- wezterm.gui is not available to the mux server, so take care to
@@ -50,52 +48,77 @@ local function scheme_for_appearance(appearance)
   end
 end
 
-return {
-  -- UI
-  adjust_window_size_when_changing_font_size = false,
-  color_scheme = scheme_for_appearance(get_appearance()),
-  enable_scroll_bar = true,
-  font = selected_font,
-  font_size = 20,
-  tab_max_width = 40,
-  use_fancy_tab_bar = true,
-  window_decorations = "INTEGRATED_BUTTONS|RESIZE",
-  window_padding = { top = 0, bottom = 0, left = 8 },
-  -- Key map
-  leader = { key = "'", mods = "CTRL", timeout_milliseconds = 1000 },
-  keys = {
-    {
-      key = "y",
-      mods = "LEADER",
-      action = act.ActivateCopyMode,
-    },
-    {
-      key = "p",
-      mods = "LEADER",
-      action = act.PasteFrom("PrimarySelection"),
-    },
-    {
-      key = "s",
-      mods = "LEADER",
-      action = act.QuickSelect,
-    },
-    {
-      key = "n",
-      mods = "LEADER",
-      action = act.PaneSelect,
-    },
-    {
-      key = "%",
-      mods = "LEADER",
-      action = act.SplitHorizontal,
-    },
-    {
-      key = '"',
-      mods = "LEADER",
-      action = act.SplitVertical,
-    },
+local color_scheme = scheme_for_appearance(get_appearance())
+
+config.adjust_window_size_when_changing_font_size = false
+config.color_scheme = color_scheme
+config.enable_scroll_bar = true
+config.font = selected_font
+config.font_size = 20
+config.tab_max_width = 40
+config.use_fancy_tab_bar = true
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.window_padding = { top = 0, bottom = 0, left = 8 }
+
+--
+-- Key mappings
+--
+
+-- Copy mode
+local copy_mode = nil
+if wezterm.gui then
+  copy_mode = wezterm.gui.default_key_tables().copy_mode
+  table.insert(
+    copy_mode,
+    { key = "[", mods = "CTRL", action = act.CopyMode("Close") }
+  )
+  table.insert(copy_mode, {
+    key = "Enter",
+    mods = "NONE",
+    action = act.Multiple({
+      { CopyTo = "ClipboardAndPrimarySelection" },
+      { CopyMode = "Close" },
+    }),
+  })
+end
+config.key_tables = { copy_mode = copy_mode }
+
+-- Leader key
+config.leader = { key = "'", mods = "CTRL", timeout_milliseconds = 1000 }
+
+-- Key maps
+config.keys = {
+  {
+    key = "y",
+    mods = "LEADER",
+    action = act.ActivateCopyMode,
   },
-  key_tables = {
-    copy_mode = copy_mode,
+  {
+    key = "p",
+    mods = "LEADER",
+    action = act.PasteFrom("PrimarySelection"),
+  },
+  {
+    key = "s",
+    mods = "LEADER",
+    action = act.QuickSelect,
+  },
+  {
+    key = "n",
+    mods = "LEADER",
+    action = act.PaneSelect,
+  },
+  {
+    key = "%",
+    mods = "LEADER",
+    action = act.SplitHorizontal,
+  },
+  {
+    key = '"',
+    mods = "LEADER",
+    action = act.SplitVertical,
   },
 }
+
+-- END
+return config
